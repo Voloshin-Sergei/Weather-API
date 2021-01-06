ea334a89666d1d50ac237cd1d9fcc1090dc17bea
Voloshin_Sergey <4voloshin@gmail.com>
2021-01-06 14:01:42 +0300

    fix: fix JS file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const wind = document.querySelector('.wind');
 const getWeather = () => {
   let city = document.querySelector('.city').value;
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3eabe3da60a0bd4f4b5a5d7d3763dc4b`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3eabe3da60a0bd4f4b5a5d7d3763dc4b`
   )
     .then(function (resp) {
       return resp.json();
